e3a618136d177cec0e19ad4ac190a7bec00e48f8
Himanshu Arora <himanshuarora171095@gmail.com>
2016-07-24 23:39:44 +0530

    First Commit

--- color.py
+++ color.py
@@ -4,6 +4,10 @@ import numpy as np
 from PIL import Image
 import cPickle
 import re
+from skimage.color import rgb2luv
+from keras.models import Sequential
+from keras.layers import Convolution2D
+
 
 numbers = re.compile(r'(\d+)')
 def numericalSort(value):
@@ -11,13 +15,18 @@ def numericalSort(value):
     parts[1::2] = map(int, parts[1::2])
     return parts
 
-def get_images(files):
-	# images = []
-	for idx , path in enumerate(files):
-		print idx
-		image = (Image.open(path)).convert('L')
-		images.append(image)
-	return images
+def preprocess(path):
+	image = np.array((Image.open(path)).convert('L'))
+	image = image.reshape((2272,1704,1))
+	truthimage = np.array(Image.open(path))
+	truthimage = rgb2luv(truthimage) #CIELUV Color Space
+	return truthimage,image
+
+def create_nn():
+
+	model = Sequential()
+	model.add(Convolution2D(64, 3,3, border_mode='same', input_shape=(2272, 1704,1),activation = 'relu'))
+	model.add(Convolution2D(64, 3,3, border_mode='same', input_shape=(2272, 1704,1),activation = 'relu'))
 
 
 
@@ -25,8 +34,7 @@ if __name__ == '__main__':
 	
 	path_to_dataset = "/home/himanshu/code/color/"
 	training_files = sorted(glob.glob(os.path.join(path_to_dataset,'Train400Img/*.jpg')) , key = numericalSort)
-
+	create_nn()
 	for idx , path in enumerate(training_files): #traverse the training images
-		image = np.array((Image.open(path)).convert('L'))
-		truthimage = np.array(Image.open(path))
+		truthimage, image = preprocess(path)
 		print image.shape , truthimage.shape
