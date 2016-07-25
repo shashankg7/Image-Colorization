3d0bc75dc27234436e7a61b3f1fee3f68bca99f3
Himanshu Arora <himanshuarora171095@gmail.com>
2016-07-25 23:58:17 +0530

    First Commit

--- color.py
+++ color.py
@@ -1,10 +1,8 @@
-import glob
-import os
+import glob , os , re
 import numpy as np
 from PIL import Image
-import cPickle
-import re
 from skimage.color import rgb2luv
+
 from keras.models import Sequential
 from keras.layers import Convolution2D
 
@@ -17,24 +15,33 @@ def numericalSort(value):
 
 def preprocess(path):
 	image = np.array((Image.open(path)).convert('L'))
-	image = image.reshape((2272,1704,1))
+	image = image.reshape((1,2272,1704))
 	truthimage = np.array(Image.open(path))
 	truthimage = rgb2luv(truthimage) #CIELUV Color Space
 	return truthimage,image
 
-def create_nn():
+def main():
 
 	model = Sequential()
-	model.add(Convolution2D(64, 3,3, border_mode='same', input_shape=(2272, 1704,1),activation = 'relu'))
-	model.add(Convolution2D(64, 3,3, border_mode='same', input_shape=(2272, 1704,1),activation = 'relu'))
+	model.add(Convolution2D(64, 3,3, border_mode='same', input_shape=(1,2272,1704),activation = 'relu'))
+	model.add(Convolution2D(64, 3,3, border_mode='same', activation = 'relu'))
 
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+	print "Model Compiled"
+
+
+	path_to_dataset = "/home/himanshu/code/color/"
+	training_files = sorted(glob.glob(os.path.join(path_to_dataset,'Train400Img/*.jpg')) , key = numericalSort)
+	for idx , path in enumerate(training_files): #traverse the training images
+		if idx < 3:
+			truthimage, image = preprocess(path)
+			print image.shape , truthimage.shape
+			model.predict(image)
 
 
 if __name__ == '__main__':
+	main()
 	
-	path_to_dataset = "/home/himanshu/code/color/"
-	training_files = sorted(glob.glob(os.path.join(path_to_dataset,'Train400Img/*.jpg')) , key = numericalSort)
-	create_nn()
-	for idx , path in enumerate(training_files): #traverse the training images
-		truthimage, image = preprocess(path)
-		print image.shape , truthimage.shape
+	
+
